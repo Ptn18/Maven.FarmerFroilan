2ae7a2b021953269a88786a07c36f5cacbe91d9a
Mark Moll <mdmoll@gmail.com>
2019-03-10 16:11:00 -0400

    Added getters and setters for main people

--- src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -6,8 +6,10 @@ public class Farmer extends Person implements Botanist, Rider {
     private Integer age;
     private Integer id;
     private Silo silo;
+    private Stable stable;
+    private Barn barn;
     
-    public Farmer(){
+    public Farmer() {
         name = "Froilan";
         age = 40;
         id = 1;
@@ -23,16 +25,25 @@ public class Farmer extends Person implements Botanist, Rider {
     
     
     public void mount(Rideable transport) {
-    
+        if (transport instanceof Animal) {
+            stable.remove((Horse) transport);
+        } else if (transport instanceof FarmVehicle) {
+            barn.remove((FarmVehicle) transport);
+        }
     }
     
     
     public void ride() {
+        System.out.println("We're ridin' along!");
     }
     
     
     public void dismount(Rideable transport) {
-    
+        if (transport instanceof Animal) {
+            stable.store((Horse) transport);
+        } else if (transport instanceof FarmVehicle) {
+            barn.store((FarmVehicle) transport);
+        }
     }
     
     public String makeNoise() {
@@ -41,14 +52,57 @@ public class Farmer extends Person implements Botanist, Rider {
     
     public void plant(Crop crop, CropRow rowNumber) {
         rowNumber.store(crop);
-        
     }
     
     public void storeEdible(Edible edible) {
         silo.store(edible);
     }
     
+    public void setStable(Stable stable) {
+        this.stable = stable;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Silo getSilo() {
+        return silo;
+    }
+    
     public void setSilo(Silo silo) {
         this.silo = silo;
+    }
+    
+    public Stable getStable() {
+        return stable;
+    }
+    
+    public Barn getBarn() {
+        return barn;
+    }
+    
+    public void setBarn(Barn barn) {
+        this.barn = barn;
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Pilot.java
@@ -1,39 +1,84 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Pilot extends Person implements Rider{
+public class Pilot extends Person implements Rider {
     
     private String name;
     private Integer age;
     private Integer id;
     Silo silo;
+    Barn barn;
     
-    public Pilot(){
+    public Pilot() {
         name = "Froilanda";
         age = 25;
         id = 1;
     }
     
     public Pilot(String name, Integer age, Integer id) {
-        super(name, age, id);}
+        super(name, age, id);
+    }
     
     public void eat(Edible food) {
         silo.remove(food);
     }
-
+    
     public String makeNoise() {
         return "Weee!";
     }
-
+    
     public void mount(Rideable transport) {
+        barn.remove((FarmVehicle) transport);
     }
-
+    
     public void ride() {
-
+        System.out.println("I'm ridin' along!");
     }
-
+    
     public void dismount(Rideable transport) {
+        barn.store((FarmVehicle) transport);
+    }
+    
+    public void fly(Aircraft aircraft) {
+        System.out.println("I'm cropdustin'!");
+    }
+    
+    public String getName() {
+        return name;
     }
 
-    public void fly(Aircraft aircraft) {
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Silo getSilo() {
+        return silo;
+    }
+    
+    public void setSilo(Silo silo) {
+        this.silo = silo;
+    }
+    
+    public Barn getBarn() {
+        return barn;
+    }
+    
+    public void setBarn(Barn barn) {
+        this.barn = barn;
     }
 }
